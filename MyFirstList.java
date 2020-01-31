77cbdcfa52fb104f255506304f4058a25ae2d2d9
Ariels33 <55289953+Ariels33@users.noreply.github.com>
2020-02-01 06:53:34 +1100

    Add files via upload

---
+++ MyFirstList.java
@@ -0,0 +1,31 @@
+package MyFirstList;
+
+import java.util.*;
+
+public class MyFirstList {
+
+	public static void main(String[] args) {
+
+		List <NUM> list = new LinkedList <NUM>();
+		
+		NUM NUM1 = new NUM (33);
+		NUM NUM2 = new NUM (45);
+		NUM NUM3 = new NUM (55);
+		
+		list.add(NUM1);
+		list.add(NUM2);
+		list.add(NUM3);
+		list.add(0, NUM3);
+		
+		for (int i = 0; i < list.size(); i++) {
+			
+			NUM a = list.get(i);
+			
+			int b = a.getNum();
+			
+			System.out.println(b);
+			
+		}
+			
+}
+}
---
+++ NUM.java
@@ -0,0 +1,16 @@
+
+package MyFirstList;
+
+public class NUM {
+    private int num;
+
+    public NUM(int i) {
+        num = i;
+    }
+
+	public int getNum() {
+		return num;
+	}
+    
+}
+
